7e8c49ea173c8f7ba05faf036c10b1b2ddf67f45
lilbillybiscuit <qianbill2014@outlook.com>
2021-09-13 02:32:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import json
 import base64
+import os
 
 def lambda_handler(event, context):
-  return "Hi"
+  #os.system("./tensorflow_chessbot.py")
+  #return "Hi"
   text=base64.b64decode(event['body'])
-  image = open("image.png", "wb")
-  image.write(base64.b64decode(data))
+  image = open("/tmp/image.png", "wb")
+  image.write(text)
   image.close()
+  os.system("./tensorflow_chessbot.py --filepath /tmp/image.png")
+  fen=open("/tmp/fen.txt", "r")
+  str1=fen.readline()
+  fen.close()
+  print("Final FEN" + str1)
+  return {
+    'statusCode': 200,
+    'body': str(str1),
+    "headers": {
+      "Access-Control-Allow-Origin" : "*",
+    }
+  }
